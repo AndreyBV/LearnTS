0117b2532d8c76a3ba83a8aedae17c7208d7b532
Andrey Barsukov <andrey.bv.gm@gmail.com>
2022-01-30 19:38:20 +0500

    Работа с классами

---
+++ classes.ts
@@ -0,0 +1,58 @@
+class Ts {
+	version: string;
+	constructor(version: string) {
+		this.version = version;
+	}
+	info(name: string) {
+		return `[${name}]: TS version is ${this.version}`;
+	}
+}
+
+// class Car {
+// 	readonly model: string;
+// 	readonly numberOfWheels: number = 4;
+
+// 	constructor(theModel: string) {
+// 		this.model = theModel; // Перезаписывать readonly поля можно только в пределах конструктора
+// 	}
+// }
+class Car {
+	readonly numberOfWheels: number = 4;
+
+	constructor(readonly model: string) {}
+}
+
+// **************
+
+class Animal {
+	protected voice: string = '';
+	public color: string = 'black'; // по умолчанию
+
+	private go() {
+		console.log('Go');
+	}
+}
+
+class Cat extends Animal {
+	public setVoice(voice: string): void {
+		this.voice = voice;
+	}
+}
+
+const cat = new Cat();
+cat.setVoice('test');
+console.log(cat.color);
+
+abstract class Component {
+	abstract render(): void;
+	abstract info(): string;
+}
+
+class AppComponent extends Component {
+	render(): void {
+		console.log('Cmp on render');
+	}
+	info(): string {
+		return 'Congratulate INFO!';
+	}
+}
